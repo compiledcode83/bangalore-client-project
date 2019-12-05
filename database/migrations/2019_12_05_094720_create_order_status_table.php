211cfb8ab5824b00c89573cc84d117e1a057edd5
residentevil0803 <ramseyklassen0803@gmail.com>
2019-12-05 14:10:13 +0300

    initial commit

---
+++ database/migrations/2019_12_05_094720_create_order_status_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrderStatusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_status', function (Blueprint $table) {
+            $table->increments('id');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_status');
+    }
+}
---
+++ database/migrations/2019_12_05_095236_create_related_products_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRelatedProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('related_products', function (Blueprint $table) {
+            $table->increments('id');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('related_products');
+    }
+}
